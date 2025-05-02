ea73c66402b6ad09db7487f13f331619879db7d9
xbradmx <152787804+xbradmx@users.noreply.github.com>
2025-05-02 13:15:23 +0100

    fied scraper for href's

--- progression projects/basic scraper.py
+++ progression projects/basic scraper.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input("Enter your url here:")
+url = str(input("Enter your url here:"))
 
 response = requests.get(url)
-response.raise_for_status  ##for bad requests 
+response.raise_for_status()  ##for bad requests 
 
-soup = BeautifulSoup (response.content, "html.parser") 
+soup = BeautifulSoup(response.content, "html.parser") 
 
 #attempting to just get phone number and email
 
-refs = soup.find_all("href")
+refs = soup.find_all("a")
 
 for reference in refs:
-  print(reference.text)
+  print(reference.get('href'))
 
 
